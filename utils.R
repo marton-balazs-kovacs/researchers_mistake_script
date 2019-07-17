73f0efe53fd56a080305ce28ac39cdfaf307a354
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2019-07-17 14:56:14 +0200

    Minor update

--- utils.R
+++ utils.R
@@ -21,7 +21,7 @@ serious_char_to_fact <- as_mapper(~ fct_relevel(.x,
                                                 "Major",
                                                 "Extreme"))
 
-# Function that transfroms frequency rating strings to integer
+#' Function that transfroms frequency rating strings to integer
 frequency_char_to_int <- as_mapper(~ case_when(.x == "Very Low" ~ 1L,
                                                .x == "Low" ~ 2L,
                                                .x == "Moderate" ~ 3L,
@@ -29,10 +29,23 @@ frequency_char_to_int <- as_mapper(~ case_when(.x == "Very Low" ~ 1L,
                                                .x == "Very High" ~ 5L,
                                                TRUE ~ NA_integer_))
 
-# Function that transforms seriousness rating strings to integer
+#' Function that transforms seriousness rating strings to integer
 serious_char_to_int <- as_mapper(~ case_when(str_detect(.x, "Insignificant") ~ 1L,
                                              str_detect(.x, "Minor") ~ 2L,
                                              str_detect(.x, "Moderate")  ~ 3L,
                                              str_detect(.x, "Major")  ~ 4L,
                                              str_detect(.x, "Extreme")  ~ 5L,
                                              TRUE ~ NA_integer_))
+
+#' Function that transforms all rating strings to integer
+rating_char_to_int <- as_mapper(~ case_when(str_detect(.x, "Insignificant") ~ 1L,
+                                            str_detect(.x, "Minor") ~ 2L,
+                                            str_detect(.x, "Moderate")  ~ 3L,
+                                            str_detect(.x, "Major")  ~ 4L,
+                                            str_detect(.x, "Extreme")  ~ 5L,
+                                            .x == "Very Low" ~ 1L,
+                                            .x == "Low" ~ 2L,
+                                            .x == "Moderate" ~ 3L,
+                                            .x == "High" ~ 4L,
+                                            .x == "Very High" ~ 5L,
+                                            TRUE ~ NA_integer_))
